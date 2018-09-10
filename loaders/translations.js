f811d1f5003e479e2a23cfcb8ae82621a8e7f3f7
Florian Renaut <florian.renaut@corp.ovh.com>
2018-09-10 11:22:42 +0200

    fix(loaders): replace console.log with emit in translations loader

--- loaders/translations.js
+++ loaders/translations.js
@@ -1,31 +1,37 @@
-function formatTranslation(object) {
+
+function formatTranslation(object, loader) {
   const result = {};
   object.forEach((elem) => {
-    if (elem.hasOwnProperty('id') && elem.hasOwnProperty('text') && !~elem.id.indexOf('>')) { // eslint-disable-line no-prototype-builtins
+    if (elem.hasOwnProperty('id') &&
+        elem.hasOwnProperty('text') &&
+        !~elem.id.indexOf('>')) { // eslint-disable-line no-prototype-builtins
       result[elem.id] = elem.text;
     } else {
-      console.error('Error: xml: format is incorrect');
+      loader.emitError(new Error(`Invalid translation xml format: '${loader.resourcePath}'`));
     }
   });
   return result;
 }
 
 module.exports = function (source) {
+
+  // translation extraction regex
   const reg = /<translation\s+id="([\w-]+?)"\s*(qtlid="([0-9]+)")?\s*(?:translate="none")?\s*?>((?:.|\n|\r)*?)<\/translation>/gi;
 
+  const obj = [];
+  let match;
 
-  const obj = []; let
-    match;
-
-  while ((match = reg.exec(source))) { // eslint-disable-line no-cond-assign
+  while (match = reg.exec(source)) { // eslint-disable-line no-cond-assign
     const elem = {
       id: match[1],
-      text: match[4].replace(/&#13;\n/g, ' ').replace(/&#160;/g, ' '),
+      text: match[4]
+        .replace(/&#13;\n/g, ' ') // carriage returns
+        .replace(/&#160;/g, ' '), // spaces
     };
 
     elem.text = elem.text.replace(/\{(\s?\d\s?)\}/g, '{{t$1}}');
-
     obj.push(elem);
   }
-  return `export default ${JSON.stringify(formatTranslation(obj))}`;
+
+  return `export default ${JSON.stringify(formatTranslation(obj, this))}`;
 };
